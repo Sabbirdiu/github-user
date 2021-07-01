0174f38c350302326c08aabc164a2598bfcafd83
sabbir hossain <sabbir.s.dk7@gmail.com>
2021-07-01 15:33:09 +0600

    added router error

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
-import Dashboard from './pages/Dashboard'
-import Login from './pages/Login';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Error from "./pages/Error";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
-      <Route path="/" exact={true} >
-        <Dashboard></Dashboard>
-      </Route>
-      <Route path='/login' >
-        <Login></Login>
-      </Route>
+      <Switch>
+        <Route path="/" exact={true}>
+          <Dashboard></Dashboard>
+        </Route>
+        <Route path="/login">
+          <Login></Login>
+        </Route>
+        <Route path="*">
+          <Error></Error>
+        </Route>
+      </Switch>
     </Router>
   );
 }
