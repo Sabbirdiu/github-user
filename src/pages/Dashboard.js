589d9364bdecbd2292ee8a0046a4fc7c1f34dc6f
sabbir hossain <sabbir.s.dk7@gmail.com>
2021-07-01 20:25:35 +0600

    added login page and components item

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
+import { Info, Repos, User, Search, Navbar } from "../components";
 
 const Dashboard = () => {
-    return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Search></Search>
+      <Info />
+      <User />
+      <Repos />
+    </div>
+  );
+};
 
-export default Dashboard
+export default Dashboard;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,11 +1,31 @@
-import React from 'react'
-
+import React from "react";
+import styled from "styled-components";
 const Login = () => {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-    )
-}
+  return (
+    <Wrapper>
+      <div className="container">
+        <img alt="github user" />
+        <h1>github user</h1>
+        <button className="btn">Log In / Sign Up</button>
+      </div>
+    </Wrapper>
+  );
+};
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  .container {
+    width: 90vw;
+    max-width: 600px;
+    text-align: center;
+  }
+  img {
+    margin-bottom: 2rem;
+  }
+  h1 {
+    margin-bottom: 1.5rem;
+  }
+`;
 
-export default Login
+export default Login;
