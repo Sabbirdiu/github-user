db0a5134f6ec5a8fe469cb06833ab730b7d02ee4
sabbir hossain <sabbir.s.dk7@gmail.com>
2021-07-01 16:01:41 +0600

    added Error Page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,11 +1,82 @@
-import React from 'react'
-
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 const Error = () => {
-    return (
-        <div>
-            <h1>Error</h1>
-        </div>
-    )
-}
+  return (
+    <Wrapper>
+      <section class="page_404">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-12 ">
+              <div class="col-sm-10 col-sm-offset-1  text-center">
+                <div class="four_zero_four_bg">
+                  <h1 class="text-center ">404</h1>
+                </div>
 
-export default Error
+                <div class="contant_box_404">
+                  <h3 class="h2">Look like you're lost</h3>
+
+                  <p>the page you are looking for not avaible!</p>
+
+                  <Link to="/" class="link_404">
+                    Go to Home
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Wrapper>
+  );
+};
+const Wrapper = styled.section`
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+  background: var(--clr-primary-10);
+  text-align: center;
+  h1 {
+    font-size: 10rem;
+  }
+  h3 {
+    color: var(--clr-grey-3);
+    margin-bottom: 1.5rem;
+  }
+  .page_404 {
+    padding: 40px 0;
+    background: #fff;
+    font-family: "Arvo", serif;
+  }
+
+  .page_404 img {
+    width: 100%;
+  }
+
+  .four_zero_four_bg {
+    background-image: url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif);
+    height: 400px;
+    background-position: center;
+  }
+
+  .four_zero_four_bg h1 {
+    font-size: 80px;
+  }
+
+  .four_zero_four_bg h3 {
+    font-size: 80px;
+  }
+
+  .link_404 {
+    color: #fff !important;
+    padding: 10px 20px;
+    background: #39ac31;
+    margin: 20px 0;
+    display: inline-block;
+    text-decoration: none;
+  }
+  .contant_box_404 {
+    margin-top: -50px;
+  }
+`;
+export default Error;
